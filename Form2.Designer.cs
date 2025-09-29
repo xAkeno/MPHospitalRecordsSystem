3b7bb6ca039607ca8ce16d1938177f9fb265b976
gms0-cereal <larsulrichbernardez@gmail.com>
2025-09-29 22:19:48 +0800

    registration form finished

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -28,12 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.txtUname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtpword = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(317, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Register";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(329, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "REGISTER";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(358, 314);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(47, 16);
+            this.linkLabel1.TabIndex = 2;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "LOGIN";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // txtUname
+            // 
+            this.txtUname.Location = new System.Drawing.Point(346, 113);
+            this.txtUname.Name = "txtUname";
+            this.txtUname.Size = new System.Drawing.Size(120, 22);
+            this.txtUname.TabIndex = 3;
+            this.txtUname.TextChanged += new System.EventHandler(this.txtUname_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(246, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "USERNAME";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(246, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "PASSWORD";
+            // 
+            // txtpword
+            // 
+            this.txtpword.Location = new System.Drawing.Point(346, 177);
+            this.txtpword.Name = "txtpword";
+            this.txtpword.Size = new System.Drawing.Size(120, 22);
+            this.txtpword.TabIndex = 6;
+            this.txtpword.TextChanged += new System.EventHandler(this.txtpword_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(188, 314);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(164, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "already have an account? ";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtpword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUname);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form2";
             this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.TextBox txtUname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtpword;
+        private System.Windows.Forms.Label label4;
     }
 }
--- Form2.cs
+++ Form2.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Google.Protobuf.WellKnownTypes;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
 
 namespace MPHospitalRecordsSystem
 {
@@ -16,5 +19,122 @@ namespace MPHospitalRecordsSystem
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            userValidation();
+        }
+
+        private void txtUname_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtpword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+        private void userValidation()
+        {
+            string user = txtUname.Text;
+            connection con = new connection();
+            using (MySqlConnection conn = con.GetConnection())
+            {
+                conn.Open();
+
+                string see = "select * from accinfo where user_name = @banana";
+                using (var cmdd = new MySqlCommand(see, conn))
+                {
+                    cmdd.Parameters.Add("@banana", MySqlDbType.VarChar).Value = user;
+                    MySqlDataReader reader = cmdd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        user = reader["user_name"].ToString();
+                        MessageBox.Show("Username already exists");
+                    }
+                    else if (user == null) { 
+                        MessageBox.Show("Username field is empty");
+                    }
+                    else
+                    {
+                        passwordValidation();
+                    }
+                    reader.Close();
+                    conn.Close();
+                }
+            }
+        }
+
+        private void passwordValidation()
+        {
+            string pass = txtpword.Text;
+            string user = txtUname.Text;
+            connection con = new connection();
+            using (MySqlConnection conn = con.GetConnection())
+            {
+                conn.Open();
+
+                var validations = new List<(bool Passed, string Message)>
+                {
+                    (pass.Any(Char.IsUpper), "This password has no uppercase letter"),
+                    (pass.Any(Char.IsDigit), "This password has no digit"),
+                    (pass.Any(c => char.IsLetterOrDigit(c)), "passowrd is less than 8 characters"),
+                    (pass== null, "pasword can't be empty"),
+
+                };
+
+                bool hasError = false;
+                foreach (var v in validations)
+                {
+                    if (!v.Passed)
+                    {
+                        MessageBox.Show(v.Message);
+                        hasError = true;
+                        break;
+
+                    }
+                }
+                string hashedPass = GetSha256Hash(pass);
+
+                if (!hasError)
+                {
+                    string query = "INSERT INTO hospital_records_db.accinfo (user_name, password) " +
+                                         "VALUES (@userName,  @password)";
+
+                    using (var cmd = new MySqlCommand(query, conn))
+                    {
+
+                        cmd.Parameters.Add("@userName", MySqlDbType.VarChar).Value = user;
+                        cmd.Parameters.Add("@password", MySqlDbType.VarChar).Value = hashedPass;
+                     
+                        int rows = cmd.ExecuteNonQuery();
+                      
+                    }
+
+
+                    Form2 frm =  new Form2();
+                    frm.Show();
+                    this.Hide();
+                }
+
+                
+            }
+        }
+        static string GetSha256Hash(string input)
+        {
+            using (SHA256 sha = SHA256.Create())
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(input);
+                byte[] hashBytes = sha.ComputeHash(bytes);
+                return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+            }
+        }
+
     }
 }
