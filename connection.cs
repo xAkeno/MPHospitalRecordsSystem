b286133b4517dc27a29e5a6c376f09889f1982af
xAkeno <clarkkentraguhos@gmail.com>
2025-09-29 12:16:03 +0800

    added all needed driver

---
+++ connection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace MPHospitalRecordsSystem
+{
+    internal class connection
+    {
+        protected static String connect = "Server=localhost; Database=hospital_records_db; Uid=root; Pwd=;";
+        public MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connect);
+        }
+    }
+}
---
+++ passwordUtils.cs
@@ -0,0 +1,22 @@
+﻿using Org.BouncyCastle.Crypto.Generators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BCrypt.Net;
+namespace MPHospitalRecordsSystem
+{
+    internal class passwordUtils
+    {
+        public string Hash(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool Verify(string password, string hashedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+        }
+    }
+}
